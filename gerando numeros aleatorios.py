4e387d83458ede383f138ae9f32fa2640b5b7c27
leandro32infotek <35671684+leandro32infotek@users.noreply.github.com>
2025-01-11 16:37:01 -0300

    Gerando números aleatórios

---
+++ gerando numeros aleatorios.py
@@ -0,0 +1,10 @@
+
+
+import random
+
+num_min = int(input("Digite o valor minimo: "))
+num_max = int(input("Digite o valor maximo: "))
+
+numero_aleatorio = random.randint(num_min,num_max)
+
+print("Numero aleatorio: ", numero_aleatorio)
